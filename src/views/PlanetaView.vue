74173367321ae5bd0f7e4b135ea65db9cebcd6a0
Jose-Macias-Code <jose.macimendo@gmail.com>
2025-05-08 16:00:58 -0600

    Primer commit: universo-vue3 base funcionando

---
+++ src/views/PlanetaView.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="planeta">
+    <h2>🪐 Planeta: {{ $route.params.id }}</h2>
+    <router-link to="/">← Volver al Home</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PlanetaView",
+};
+</script>
